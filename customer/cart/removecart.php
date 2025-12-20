53d73c86c2365ca25e2a349837b23b35b899916b
Jana-Elwalily <jana.elwalily@icloud.com>
2025-12-20 04:15:56 +0200

    Update removecart.php

--- customer/cart/removecart.php
+++ customer/cart/removecart.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../order_fn.php';
 header('Content-Type: application/json');
-session_start();
-
 $customer_id = $_SESSION['customer_id'] ?? null;
 if (!$customer_id) {
     echo json_encode(['success' => false, 'message' => 'Not logged in']);
@@ -13,4 +11,5 @@ $data = json_decode(file_get_contents("php://input"), true);
 $isbn = $data['ISBN'] ?? '';
 
 echo json_encode(removeFromCart($customer_id, $isbn));
+
 ?>
