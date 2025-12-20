8fad0c8225182d93c1cd50c8f8f3a230ecd6e9d7
Jana-Elwalily <jana.elwalily@icloud.com>
2025-12-20 04:15:34 +0200

    Update clear.php

--- customer/cart/clear.php
+++ customer/cart/clear.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../order_fn.php';
 header('Content-Type: application/json');
-session_start();
-
 $customer_id = $_SESSION['customer_id'] ?? null;
 if (!$customer_id) {
     echo json_encode(['success' => false, 'message' => 'Not logged in']);
@@ -10,4 +8,5 @@ if (!$customer_id) {
 }
 
 echo json_encode(clearCart($customer_id));
+
 ?>
