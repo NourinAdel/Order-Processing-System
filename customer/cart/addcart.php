cab207582f71bc35a29261041d4ed7c406b04050
Jana-Elwalily <jana.elwalily@icloud.com>
2025-12-20 04:14:14 +0200

    Update addcart.php

--- customer/cart/addcart.php
+++ customer/cart/addcart.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../order_fn.php';
 header('Content-Type: application/json');
-session_start();
-
 $customer_id = $_SESSION['customer_id'] ?? null;
 if (!$customer_id) {
     echo json_encode(['success' => false, 'message' => 'Not logged in']);
@@ -14,4 +12,5 @@ $isbn = $data['ISBN'] ?? '';
 $quantity = $data['quantity'] ?? 1;
 
 echo json_encode(addToCart($customer_id, $isbn, $quantity));
+
 ?>
