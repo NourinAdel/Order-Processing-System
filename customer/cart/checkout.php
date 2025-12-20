03e961e61eabcdb4fa4eb1486174bafeaed65f85
Jana-Elwalily <jana.elwalily@icloud.com>
2025-12-20 04:14:36 +0200

    Update checkout.php

--- customer/cart/checkout.php
+++ customer/cart/checkout.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../order_fn.php';
 header('Content-Type: application/json');
-session_start();
-
 $customer_id = $_SESSION['customer_id'] ?? null;
 if (!$customer_id) {
     echo json_encode(['success' => false, 'message' => 'Not logged in']);
@@ -19,3 +17,4 @@ echo json_encode(
     )
 );
 ?>
+
