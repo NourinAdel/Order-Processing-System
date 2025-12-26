da21fee0b2dbae3d041e4b6e5d785599b81fbefe
Jana-Elwalily <jana.elwalily@icloud.com>
2025-12-26 17:06:39 +0200

    fix order history

--- customer/cart/order_history.php
+++ customer/cart/order_history.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../order_fn.php';
+session_start();
 header('Content-Type: application/json');
 $customer_id = $_SESSION['customer_id'] ?? null;
 if (!$customer_id) {
@@ -7,5 +8,9 @@ if (!$customer_id) {
     exit;
 }
 
-echo json_encode(getOrderHistory($customer_id));
+$orders= getOrderHistory($customer_id);
+echo json_encode([
+    'success' =>true,
+    'orders' => $orders
+]):
 ?>
